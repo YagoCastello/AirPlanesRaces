713e0f30bc34321cd3559f919af9ea0760e10dd0
Yago Castello <yagodealmeidacs@gmail.com>
2026-06-22 16:49:53 -0300

    feat controller race

--- src/main/java/java10x/dev/AirPlanesRaces/AirPLANES/AirplaneController.java
+++ src/main/java/java10x/dev/AirPlanesRaces/AirPLANES/AirplaneController.java
@@ -1,8 +1,6 @@
 package java10x.dev.AirPlanesRaces.AirPLANES;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,4 +9,43 @@ public class AirplaneController {
     public String boasvindas(){
     return "Boas vindas AIRPLANE";
 }
+
+
+    // Adicionar Airplane (CREATE)
+    @PostMapping("/criar")
+    public String criarAirplane(){
+        return "Airplane criado";
+    }
+
+// Mostar Airplane por ID(READ)
+@GetMapping("/todosID")
+    public String listarTodosID(){
+    return "VARIOS AIRPLANES POR ID";
 }
+// Mostrar todos os Airplanes(READ)
+@GetMapping("/todos")
+public String listarTodos(){
+    return "VARIOS AIRPLANES";
+}
+// Alterar dados dos Airplanes(UPDATE)
+@PutMapping("/alterarid")
+    public String alterarAirplaneId(){
+    return "ALTERAR AIRPLANEEEEE";
+}
+
+// Deletar Airplane(DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarAirplane(){
+    return "DELETAR AIRPLANE";
+    }
+
+
+
+
+
+
+
+
+
+}
+
--- src/main/java/java10x/dev/AirPlanesRaces/RACES/RaceController.java
+++ src/main/java/java10x/dev/AirPlanesRaces/RACES/RaceController.java
@@ -1,15 +1,30 @@
 package java10x.dev.AirPlanesRaces.RACES;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class RaceController {
 
-    @GetMapping("/boasvindass")
-    public String boasvindass(){
-        return "Boas vindas RACE";
+    @GetMapping("/listar")
+    public String listarRace() {
+        return "Listando RACE";
     }
+
+    @PostMapping("/criar")
+    public String CriarRace(){
+        return "Criando uma race";
 }
+
+@PutMapping("/alterar")
+    public String alterarRace(){
+        return  "RACE alterada com sucesso";
+
+}
+@DeleteMapping("/deletar")
+    public  String deletarRace(){
+        return "RACE deletada!";
+}
+
+}
+
